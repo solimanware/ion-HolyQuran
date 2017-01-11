fbe3bb32fbc3dbfed6ea890517aa0e58561e66e7
Idrees Khan <idrees@fi.ae>
2017-01-11 09:05:34 +0400

    fixed item height warning and other minor stuffs

--- src/pages/surah/surah.page.ts
+++ src/pages/surah/surah.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { SurahService } from './surah.service';
+import { BookmarkService } from '../bookmark/bookmark.service';
+import { Bookmark } from '../bookmark/bookmark';
 import { Sura } from './sura';
 import { VersePage } from '../verse/verse.page';
 import { VerseParams } from '../verse/verse';
@@ -13,13 +15,22 @@ import { VerseParams } from '../verse/verse';
 export class SurahPage {
     public surahs: Array<Sura> = [];
 
-    constructor(private navCtrl: NavController, private surahService: SurahService) {
+    constructor(private navCtrl: NavController,
+        private surahService: SurahService, private bookmarkService: BookmarkService) {
     }
 
     ionViewWillEnter() {
         this.surahService.getAll().then((surahs: Array<Sura>) => {
             this.surahs = surahs;
         });
+            // .then(() => {
+            //     this.bookmarkService.getAllApplicationBookmarks()
+            //         .then((bookmarks: Array<Bookmark>) => {
+            //             if(bookmarks.length > 0){
+            //                 let bookmark = bookmarks[0];
+            //             }
+            //         });
+            // });
     }
 
     goToVerses(sura: Sura) {
